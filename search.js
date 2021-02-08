d145d5c2cc4d14b675590f4a0a01efe9b0e0a762
williamsondv <williamsondavidv@outlook.com>
2021-02-08 10:13:15 -0600

    remove console.log statements, change search bar text color and eliminate a null reference error on search bar event listener.

--- search.js
+++ search.js
@@ -7,11 +7,10 @@ function searchFunction() {
    let filter = input.value.toUpperCase();
    let thumb = document.getElementsByTagName("a");
    let searchString;
+
     for(let i = 0; i < 12; i++) {
         searchString = thumb[i].getAttribute("data-caption").toString().toUpperCase();
-        console.log(typeof(searchString));
         if(searchString.includes(filter)) {
-            console.log(filter);
             thumb[i].style.display = "block";
         } else {
             thumb[i].style.display = "none";
@@ -21,8 +20,7 @@ function searchFunction() {
    
 }
 
-let initialInput = document.getElementById("search-bar");
-initialInput.addEventListener("onkeyup", searchFunction());
+
 
 
   
